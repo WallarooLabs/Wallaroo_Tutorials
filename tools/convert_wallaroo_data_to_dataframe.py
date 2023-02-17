f9031261ad5de558b7736f7abd38e1fa8841bb6a
johnhummelWallaroo <john.hummel@wallaroo.ai>
2023-02-17 12:10:57 -0700

    Readded conversion script.

--- tools/convert_wallaroo_data_to_dataframe.py
+++ tools/convert_wallaroo_data_to_dataframe.py
@@ -54,11 +54,46 @@ jsonFileList = [
     #     "arrowOutputFile": "wallaroo-model-cookbooks/imdb/data/test_data.arrow"
     # },
     # {
-    #     "inputFile": "wallaroo-model-cookbooks/imdb/data/tokenized50K.json",
-    #     "dataframeOutputFile": "wallaroo-model-cookbooks/imdb/data/tokenized50K.df.json",
-    #     "arrowOutputFile": "wallaroo-model-cookbooks/imdb/data/tokenized50K.arrow"
+    #     "inputFile": "wallaroo-model-cookbooks/imdb/data/test_data_50K.json",
+    #     "dataframeOutputFile": "wallaroo-model-cookbooks/imdb/data/test_data_50K.df.json",
+    #     "arrowOutputFile": "wallaroo-model-cookbooks/imdb/data/test_data_50K.arrow"
+    # },
+    # {
+    #     "inputFile": "wallaroo-testing-tutorials/abtesting/data/data-1.json",
+    #     "dataframeOutputFile": "wallaroo-testing-tutorials/abtesting/data/data-1.df.json",
+    #     "arrowOutputFile": "wallaroo-testing-tutorials/abtesting/data/data-1.arrow"
+    # },
+    # {
+    #     "inputFile": "wallaroo-testing-tutorials/abtesting/data/data-1k.json",
+    #     "dataframeOutputFile": "wallaroo-testing-tutorials/abtesting/data/data-1k.df.json",
+    #     "arrowOutputFile": "wallaroo-testing-tutorials/abtesting/data/data-1k.arrow"
+    # },
+    # {
+    #     "inputFile": "wallaroo-testing-tutorials/abtesting/data/data-25k.json",
+    #     "dataframeOutputFile": "wallaroo-testing-tutorials/abtesting/data/data-25k.df.json",
+    #     "arrowOutputFile": "wallaroo-testing-tutorials/abtesting/data/data-25k.arrow"
     # }
-    
+    # {
+    #     "inputFile": "wallaroo-101/data/cc_data_1k.json",
+    #     "dataframeOutputFile": "wallaroo-101/data/cc_data_1k.df.json",
+    #     "arrowOutputFile": "wallaroo-101/data/cc_data_1k.arrow"
+    # },
+    # {
+    #     "inputFile": "wallaroo-101/data/high_fraud.json",
+    #     "dataframeOutputFile": "wallaroo-101/data/high_fraud.df.json",
+    #     "arrowOutputFile": "wallaroo-101/data/high_fraud.arrow"
+    # },
+    {
+        "inputFile": "wallaroo-101/data/smoke_test.json",
+        "dataframeOutputFile": "wallaroo-101/data/smoke_test.df.json",
+        "arrowOutputFile": "wallaroo-101/data/smoke_test.arrow"
+    },
+    # {
+    #     "inputFile": "wallaroo-101/data/cc_data_10k.json",
+    #     "dataframeOutputFile": "wallaroo-101/data/cc_data_10k.df.json",
+    #     "arrowOutputFile": "wallaroo-101/data/cc_data_10k.arrow"
+    # }
+   
 ]
 
 def ConvertJSONtoDataframe(inputFile, outputDataFrameFile):
@@ -71,41 +106,43 @@ def ConvertJSONtoDataframe(inputFile, outputDataFrameFile):
     return data
 
 # input is a dataframe
-def ConvertJSONtoArrow(inputDataFrame, outputArrowFile):
-    data_table = pa.Table.from_pandas(inputDataFrame)
-    #data_schema = pa.Schema.from_pandas(inputDataFrame)
+def ConvertJSONtoArrow(inputFile, outputArrowFile):
+    # starting with the original input file
+    data =  pd.read_json(inputFile, orient="records")
+    data_table = pa.Table.from_pandas(data)
+    data_schema = pa.Schema.from_pandas(data)
     fields = []
     for i in data_table.column_names:
         if pa.types.is_fixed_size_list(data_table[i].type):
             fields.append(pa.field(i, data_table[i].type))
         else:
-            # print(data_table[i])
+            #print(data_table[i])
             inner_size = len(data_table[i][0])
             tensor_type = {"shape": [inner_size]}
             tensor_meta_type = {"tensor_type": json.dumps(tensor_type)}
             tensor_arrow_type = pa.list_(data_table[i][0][0].type, inner_size)
             fields.append(pa.field(i, tensor_arrow_type, metadata=tensor_meta_type))
-    
+        
     schema = pa.schema(fields)
-
-    final_table = pa.Table.from_pandas(inputDataFrame, schema=schema)
-
-    # save the final_table as an arrow binary file
-
-    with pa.OSFile(outputArrowFile, 'wb') as sink:
+    final_table = pa.Table.from_pandas(data, schema=schema).cast(target_schema=schema)
+    #print(final_table.schema)
+    arrow_file_name = outputArrowFile
+    with pa.OSFile(arrow_file_name, 'wb') as sink:
         with pa.ipc.new_file(sink, final_table.schema) as arrow_ipc:
             arrow_ipc.write(final_table)
             arrow_ipc.close()
-    # this is an arrow object
     return final_table
 
 def main():
     # convert all of the JSON files to dataframe and arrow
     for currentFile in jsonFileList:
         newDataFrame = ConvertJSONtoDataframe(currentFile["inputFile"], currentFile["dataframeOutputFile"])
-        print(newDataFrame)
-        newArrow = ConvertJSONtoArrow(newDataFrame, currentFile["arrowOutputFile"])
+        #print(newDataFrame)
+        newArrow = ConvertJSONtoArrow(currentFile["inputFile"], currentFile["arrowOutputFile"])
+        print("table:")
         print(newArrow)
+        print("Schema:")
+        print(newArrow.schema)
     
 
 
