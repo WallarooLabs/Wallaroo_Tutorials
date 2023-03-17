87f47504eb374238bbbf603aa3183da41ca41e20
johnhummelWallaroo <john.hummel@wallaroo.ai>
2023-03-17 16:01:57 -0600

    Updated for 2023.2 arrow.

---
+++ scripts/convert_data_to_arrow.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pyarrow as pa
+import json
+import convert_wallaroo_data
+
+# setting arrow on or off
+#import os
+#os.environ["ARROW_ENABLED"]="False"
+
+# List of files to convert
+jsonFileList = [
+    {
+        "inputFile": "wallaroo-101/data/cc_data_1k.df.json",
+        "arrowOutputFile": "wallaroo-101/data/cc_data_1k.arrow"
+    }
+
+]
+
+def main():
+    # convert all of the JSON files to dataframe and arrow
+    for currentFile in jsonFileList:
+        currentDataFrame = pd.read_json(currentFile['inputFile'], orient="records")
+        newArrowTable = convert_wallaroo_data.convert_pandas_to_arrow(currentDataFrame, None)
+        with pa.OSFile(currentFile['arrowOutputFile'], 'wb') as sink:
+            with pa.ipc.new_file(sink, newArrowTable.schema) as arrow_ipc:
+                arrow_ipc.write(newArrowTable)
+                arrow_ipc.close()
+        #print(newDataFrame)
+        # newArrow = ConvertJSONtoArrow(currentFile["inputFile"], currentFile["arrowOutputFile"])
+        # print("table:")
+        # print(newArrow)
+        # print("Schema:")
+        # print(newArrow.schema)
+
+
+
+if __name__ == '__main__':
+    main()
