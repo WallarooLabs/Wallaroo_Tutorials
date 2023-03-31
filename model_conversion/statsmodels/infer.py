8b2214b4a7189e1db52ff3e38048fecb2040665a
johnhummelWallaroo <john.hummel@wallaroo.ai>
2023-03-31 11:49:40 -0600

    arrow updates.

--- model_conversion/statsmodels/infer.py
+++ model_conversion/statsmodels/infer.py
@@ -1,10 +1,13 @@
 import json
 import pandas as pd
 
+# What if we just change data to a DataFrame.
 def wallaroo_json(data):
-    obj = json.loads(data)
-    evaluation_frame = pd.DataFrame.from_dict(obj)
+    # obj = json.loads(data)
+    # evaluation_frame = pd.DataFrame.from_dict(obj)
+    evaluation_frame = data
     extra_regressors = ["temp", "holiday", "workingday", "windspeed"]
     forecast = model.forecast(steps=7, exog=evaluation_frame.loc[:, extra_regressors])
 
-    return {"forecast": forecast.tolist()}
+    # return {"forecast": forecast.tolist()}
+    return {"forecast": forecast}
