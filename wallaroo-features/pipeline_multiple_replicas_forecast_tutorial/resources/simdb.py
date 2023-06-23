375b30eb7b406ee29fd679fab763ecd005e8de4f
johnhansarickWallaroo <john.hummel@wallaroo.ai>
2023-06-23 08:44:38 -0600

    forecast demo

---
+++ wallaroo-features/pipeline_multiple_replicas_forecast_tutorial/resources/simdb.py
@@ -0,0 +1,19 @@
+# A module to simulate having a data base or other data repository to query from
+
+import sqlite3
+import pandas as pd
+import numpy as np
+import datetime
+
+_datafile = "./day.csv"
+tablename = "bikerentals"
+
+# return a simulated database connection that is backed by datafile
+def get_db_connection(datafile =_datafile, tablename=tablename):
+    df = pd.read_csv(datafile)
+    df['date'] = df['dteday']
+    
+    conn = sqlite3.connect(":memory:")
+    df.to_sql(tablename, conn, index=False)
+    return conn
+
