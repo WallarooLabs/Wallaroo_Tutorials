1b2f1e8743d954c17d10b39c5329ba8e68be1cb3
John Hansarick <john.hummel@wallaroo.ai>
2023-07-06 14:18:53 -0600

    pipeline step logging tutorial

--- model_uploads/python/models/step.py
+++ model_uploads/python/models/step.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# take a dataframe output of the house price model, and reformat the `dense_2`
+# column as `output`
 def wallaroo_json(data: pd.DataFrame):
     print(data)
     return [{"output": [data["dense_2"].to_list()[0]]}]
