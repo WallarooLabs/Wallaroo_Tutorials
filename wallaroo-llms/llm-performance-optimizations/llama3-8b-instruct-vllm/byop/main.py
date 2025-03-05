70c7a557efb4ec19510bdcbbced17c6961aa46ac
yamarwallaroo11 <98778131+yamarwallaroo11@users.noreply.github.com>
2025-03-05 11:29:16 -0500

    Update main.py

--- wallaroo-llms/llm-performance-optimizations/llama3-8b-instruct-vllm/byop/main.py
+++ wallaroo-llms/llm-performance-optimizations/llama3-8b-instruct-vllm/byop/main.py
@@ -10,7 +10,7 @@ from mac.inference.creation import InferenceBuilder
 from mac.types import InferenceData
 
 pip_command = (
-    f'{sys.executable} -m pip install https://github.com/vllm-project/vllm/releases/download/v0.5.2/vllm-0.5.2+cu118-cp310-cp310-manylinux1_x86_64.whl --extra-index-url https://download.pytorch.org/whl/cu118'
+    f'{sys.executable} -m pip install https://github.com/vllm-project/vllm/releases/download/v0.7.3/vllm-0.7.3+cu121-cp310-cp310-manylinux1_x86_64.whl --extra-index-url https://download.pytorch.org/whl/cu121'
 )
 
 subprocess.check_call(pip_command, shell=True)
