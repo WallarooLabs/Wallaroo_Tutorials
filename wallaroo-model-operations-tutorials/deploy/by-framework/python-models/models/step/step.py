0aa128f590a8868ebf10e06fcd282b5fd3066d4c
john@hansarick.com <john@hansarick.com>
2026-02-18 13:20:42 -0700

    updated python tutorial

--- wallaroo-model-operations-tutorials/deploy/by-framework/python-models/models/step/step.py
+++ wallaroo-model-operations-tutorials/deploy/by-framework/python-models/models/step/step.py
@@ -8,7 +8,6 @@ from mac.types import InferenceData
 
 def process_data(input_data: InferenceData) -> InferenceData:
     # just changing the output data field
-    # input_data['output'] = input_data.pop('dense_2')
     return {
         'output': np.float32(input_data.pop('dense_2'))
     }
